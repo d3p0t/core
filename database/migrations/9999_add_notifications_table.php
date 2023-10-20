b8e3372e342b312509c0e7b0bc12655fb07b30c2
Pascal Wilbrink <pascal.wilbrink@alliander.com>
2023-10-20 20:14:35 +0200

    Removed notification, notifiable should be implemented

--- database/migrations/9999_add_notifications_table.php
+++ database/migrations/9999_add_notifications_table.php
@@ -12,14 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('core__notifications', function (Blueprint $table) {
-            $table->id();
-
-            $table->string('subject')->nullable();
-            $table->string('content');
-            $table->boolean('is_read')->default(false);
-
-            $table->morphs('recipient');
-
+            $table->uuid('id')->primary();
+            $table->string('type');
+            $table->morphs('notifiable');
+            $table->text('data');
+            $table->timestamp('read_at')->nullable();
             $table->timestamps();
         });
     }
